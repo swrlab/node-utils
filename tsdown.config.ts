7328bd963a5c3b9a544ccae230eb8f60ad9ff4c9
Pascal Weiland <weiland@users.noreply.github.com>
2025-09-06 18:02:58 +0200

    chore: upgrade dependencies

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
 	entry: {
 		index: 'src/index.ts',
 		ard: 'src/ard/index.ts',
-		dates: 'src/dates/index.ts',
+		dates: 'src/date/index.ts',
+		string: 'src/string/index.ts',
+		number: 'src/number/index.ts',
 	},
 })
