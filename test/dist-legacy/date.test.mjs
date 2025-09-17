1ae37d5da074b7f93c9797066bcad14bdaea5dbc
Pascal Weiland <weiland@users.noreply.github.com>
2025-09-17 20:19:35 +0200

    chore(test): rename tests to test
    and add dist-legacy tests

---
+++ test/dist-legacy/date.test.mjs
@@ -0,0 +1,66 @@
+import { describe, it } from 'node:test'
+import * as date from '../../dist/packages/date.js'
+import { expect } from './utils.mjs'
+
+const testDate = '2038-01-19T03:14:08.000'
+const relativeTime = 2147483647000 - Date.now()
+const relativeYears = Number.parseInt(relativeTime / (1000 * 60 * 60 * 24 * 365), 10)
+
+describe('Test DateTime Package', () => {
+	describe('Test getDateHourMinutes', () => {
+		const testResult = 'Di, 19. Januar 2038 - 03:14 Uhr'
+		it(`getDateHourMinutes('${testDate}') = '${testResult}'`, () => {
+			expect(date.getDateHourMinutes(testDate)).to.equal(testResult)
+		})
+	})
+
+	describe('Test getDayMonthYear', () => {
+		const testResult = 'Di, 19. Januar 2038'
+		it(`getDayMonthYear('${testDate}') = '${testResult}'`, () => {
+			expect(date.getDayMonthYear(testDate)).to.equal(testResult)
+		})
+	})
+
+	describe('Test getFullRelativeTime', () => {
+		const testResult = `Di, 19. Januar 2038 - 03:14 Uhr (in ${relativeYears} Jahren)`
+		it(`getFullRelativeTime('${testDate}') = '${testResult}'`, () => {
+			expect(date.getFullRelativeTime(testDate)).to.equal(testResult)
+		})
+	})
+
+	describe('Test getHourMinutes', () => {
+		const testResult = '03:14'
+		it(`getHourMinutes('${testDate}') = '${testResult}'`, () => {
+			expect(date.getHourMinutes(testDate)).to.equal(testResult)
+		})
+	})
+
+	describe('Test getIsoRelativeTime', () => {
+		const testResult = `${testDate} (in ${relativeYears} Jahren)`
+		it(`getIsoRelativeTime('${testDate}') = '${testResult}'`, () => {
+			expect(date.getIsoRelativeTime(testDate)).to.equal(testResult)
+		})
+	})
+
+	describe('Test getRelativeTime', () => {
+		const testResult = `in ${relativeYears} Jahren`
+		it(`getRelativeTime('${testDate}') = '${testResult}'`, () => {
+			expect(date.getRelativeTime(testDate)).to.equal(testResult)
+		})
+	})
+
+	describe('Test getYearMonthDay', () => {
+		const testResult = '20380119'
+		it(`getYearMonthDay('${testDate}') = '${testResult}'`, () => {
+			expect(date.getYearMonthDay(testDate)).to.equal(testResult)
+		})
+	})
+
+	describe('Test revYearMonthDay', () => {
+		const test = '20380119'
+		const testResult = '19012038'
+		it(`revYearMonthDay('${test}') = '${testResult}'`, () => {
+			expect(date.revYearMonthDay(test)).to.equal(testResult)
+		})
+	})
+})
