46b14e58b6e9e07842e9f4fba3db4ab0bb1f37bc
Pascal Weiland <weiland@users.noreply.github.com>
2026-03-18 16:15:58 +0100

    chore(utils-legacy): always bundle the storage wrapper

--- packages/utils-legacy/tsdown.config.ts
+++ packages/utils-legacy/tsdown.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
+	deps: {
+		alwaysBundle: ['@swrlab/storage'],
+	},
 	entry: {
 		index: 'src/index.ts',
 		'packages/*': 'src/*.ts',
