4e567246759318f0e508f8d8a97f855a2c7148d9
Pascal Weiland <weiland@users.noreply.github.com>
2026-07-23 17:26:09 +0200

    feat(string): support TextEncoder for converting to base64
    `TextEncoder` along with `<Uint8Array>.toBase64` works only in the browser
    but does not use Node.js' Buffer.

--- packages/utils/src/string/base64.ts
+++ packages/utils/src/string/base64.ts
@@ -5,7 +5,19 @@ import { Buffer } from 'node:buffer'
  * @param str - The input string encoded in utf-8.
  * @returns - The base64 encoded string.
  */
-export const toBase64 = (str: string): string => Buffer.from(str, 'utf-8').toString('base64')
+const _toBase64Buffer = (str: string): string => Buffer.from(str, 'utf-8').toString('base64')
+
+/**
+ * Encode a given UTF-8 string to bytes.
+ *
+ * @param {string} str - The input string.
+ * @returns {Uint8Array} - bytes
+ */
+export const toBase64 = (str: string): string =>
+	// `.toBase64` is only available in browsers (but Buffer is not, since it is Node.js API).
+	typeof TextEncoder !== 'undefined' && typeof new Uint8Array().toBase64 !== 'undefined'
+		? new TextEncoder().encode(str).toBase64()
+		: _toBase64Buffer(str)
 
 /**
  * Decode the given base64 string.
